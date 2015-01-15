c93071ec5707c6bd384758255bf10fc7cf5f7b07
Arne Schlüter <aesthaddicts@gmail.com>
2015-01-15 18:54:16 +0100

    Re-word and start with training

--- classify.py
+++ classify.py
@@ -1,4 +1,6 @@
 import glob
+from collections import defaultdict
+
 from document import Document
 
 # first, set up training data
@@ -9,3 +11,8 @@ for file_path in training_files:
     # the class is encoded into the file_path (first * in patttern above)
     document_class = file_path.split("/")[2]
     training_docs.append(Document(file_path, document_class))
+
+# cache this for later use
+docs_in_class = defaultdict(list)
+for doc in training_docs:
+    docs_in_class[doc.type].append(doc)
--- document.py
+++ document.py
@@ -1,11 +1,11 @@
 class Document():
     """Represents a document on the filesystem"""
-    
+
     def __init__(self, file_path, type=None, encoding="utf-8"):
         file = open(file_path, encoding=encoding)
         self.type = type
         self.content = file.read()
         file.close()
-        
-    def get_vocabulary(self):
+
+    def get_tokens(self):
         return self.content.lower().split()
