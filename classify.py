fca5dd13d8634aa91144f2f08688dc1345742ed7
Arne Schlüter <aesthaddicts@gmail.com>
2015-01-15 18:21:14 +0100

    Clearify wording in classify.py

--- classify.py
+++ classify.py
@@ -7,5 +7,5 @@ training_docs = []
 
 for file_path in training_files:
     # the class is encoded into the file_path (first * in patttern above)
-    file_class = file_path.split("/")[2]
-    training_docs.append(Document(file_path, file_class))
+    document_class = file_path.split("/")[2]
+    training_docs.append(Document(file_path, document_class))
